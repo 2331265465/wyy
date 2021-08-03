6e9c1848e3888e50a86e0c424df13eec32d36d2f
幻樱 <xiebo121314@gmail.com>
2021-08-03 22:06:34 +0800

    完成底部播放器布局

---
+++ src/app/share/wy-ui/wy-player/wy-player.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WyPlayerComponent } from './wy-player.component';
+
+
+
+@NgModule({
+  declarations: [WyPlayerComponent],
+  imports: [
+  ],
+  exports:[WyPlayerComponent]
+})
+export class WyPlayerModule { }
