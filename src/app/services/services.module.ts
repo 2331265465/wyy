405c84e51d3da4e3aa825bf48374650d40f71b2f
幻樱 <xiebo121314@gmail.com>
2021-09-16 22:47:57 +0800

    删除了不必要的内容

--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -1,14 +1,14 @@
 import {InjectionToken, NgModule} from '@angular/core';
 import {httpInterceptorProviders} from './http-interceptors';
+import {environment} from "../../environments/environment.prod";
 
 export const API_CONFIG = new InjectionToken('ApiConfigToken');
-export const WINDOW = new InjectionToken('WindowToken');
 
 @NgModule({
   declarations: [],
   imports: [],
   providers: [
-    {provide: API_CONFIG, useValue: '/api/'},
+    {provide: API_CONFIG, useValue: environment.production ? '/' : '/api/'},
     httpInterceptorProviders
   ]
 })
